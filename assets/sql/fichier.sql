ad7244bd79243aa931ac947e6d41cb6cf8f31028
Lucastor05 <lucasdeniel05@gmail.com>
2022-11-04 12:44:06 +0100

    Finito sql

--- assets/sql/fichier.sql
+++ assets/sql/fichier.sql
@@ -16,11 +16,11 @@ INSERT INTO `Utilisateur` (`Email`, `Mot_de_passe`, `Pseudo`, `Date_et_heure_ins
 
 UPDATE Utilisateur
 SET Mot_de_passe = 'nouveau mdp'
-WHERE Identifiant =' id user'
+WHERE Identifiant =iduser
 
 UPDATE Utilisateur
 SET Email = 'nv mail'
-WHERE Identifiant = 'id user' AND Mot_de_passe = mdpEntrezDansForm
+WHERE Identifiant = iduser AND Mot_de_passe = mdpEntrezDansForm
 
 /* Story 4 */
 
@@ -40,7 +40,7 @@ SELECT Jeu.Nom_du_jeu, Utilisateur.Pseudo, Score.Difficulte_de_la_partie, Score.
 FROM Score
 INNER JOIN Jeu ON Score.Identifiant_du_jeu = Jeu.Identifiant 
 INNER JOIN Utilisateur ON Utilisateur.Identifiant = Score.Identifiant_du_joueur
-ORDER BY Jeu.Nom_du_jeu ASC, Score.Difficulte_de_la_partie, Score.Score_de_la_partie
+ORDER BY Jeu.Nom_du_jeu, Score.Difficulte_de_la_partie, Score.Score_de_la_partie
 
 
 /* Story 7 */
@@ -63,11 +63,17 @@ SET Score_de_la_partie = 'nouveau score'
 WHERE Identifiant =' id user'
 
 /* Si existe pas */
-INSERT INTO `Score` (`Score_de_la_partie`) 
-    VALUES ('NouveauScore');
+INSERT INTO `Score` (`Identifiant_du_joueur`, `Identifiant_du_jeu`, `Difficulte_de_la_partie`, `Score_de_la_partie`, `Date_et_heure_de_la_partie`) 
+    VALUES (idUser, gameID, GameDifficulty, 'NouveauScore', NOW());
+
 
 /* Story 9 */
 
-INSERT INTO `Message` (`Identifiant`, `Identifiant_du_jeu`, `Identifiant_de_expéditeur`, `Message`, `Date_et_heure_du_message`) VALUES (NULL, '2', '3', 'Hello World', '2022-11-01');
+INSERT INTO `Message` (`Identifiant`, `Identifiant_du_jeu`, `Identifiant_de_expéditeur`, `Message`, `Date_et_heure_du_message`) VALUES (NULL, GameID, UserID, MessageUser, NOW());
 
 /* Story 10 */
+
+SELECT Message.Identifiant_de_expediteur , Message.Message, Message.Date_et_heure_du_message
+FROM Message 
+WHERE Message.Date_et_heure_du_message >= NOW() - INTERVAL 1 DAY;
+
