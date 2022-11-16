8a180c5792b43503576bbd84ac88e1ec6f5ecdf2
victor sullivan <victorsullivan@MacBook-Pro-de-admin.local>
2022-11-16 11:19:24 +0100

    story5 fini pour les deux formulaires de la page myaccount

--- myaccount.php
+++ myaccount.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 include 'assets/includes/database.inc.php';
+
+
+
+$idUserMyAccount = 4;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -43,75 +48,102 @@ include 'assets/includes/database.inc.php';
             <label for="Cpassword"></label><br>
             <input type="password" id="Cpassword" name="Cpassword" placeholder ="Confirmez le mot de passe" required="Cpassword" class="formulaire-myaccount"><br>
             <button name="MAJEmail" type="submit" value="HTML" class="MAJEmailButton-myaccount">Mise à jour</button>
-        </form>
-
-        <p align="center">
-		<?php
-		if (!empty($_POST)) {
-			$point = strpos($_POST['ancienEmail'], ".");
-			$aroba = strpos($_POST['ancienEmail'], "@");
-			if ($point === false)
-				echo 'Votre ancien Email doit comporter un point.';
-			else if ($aroba === false)
-				echo 'Votre ancien Email doit comporter un arobase.';
-			else
-				echo 'Votre ancien Email  ' . $_POST['ancienEmail'] . ' est valide.';
-		} else {
-			echo 'Veuillez saisir votre ancien Email dans le champ prévu à cette effet.';
-		}
-		?>
-		</p>
-
-        <p align="center">
+        </form>		
         <?php
-		if (!empty($_POST)) {
-			$point = strpos($_POST['nouvelEmail'], ".");
-			$aroba = strpos($_POST['nouvelEmail'], "@");
-			if ($point === false)
-				echo 'Votre nouvel Email doit comporter un point.';
-			else if ($aroba === false)
-				echo 'Votre nouvel Email doit comporter un arobase.';
-			else
-				echo 'Votre nouvel Email ' . $_POST['nouvelEmail'] . ' est valide.';
-		} else {
-			echo 'Veuillez saisir un  nouvel Email dans le champ prévu à cette effet.';
-		}
-		?>
-		</p>
 
-        <?php
-        $mdplength = strlen($_POST['password']);
-            if($mdplength >= 8)
-            {
-                 }
-            else
-            {
+        $erreur = false;
+
+
+		if (isset($_POST['MAJEmail'])) {
+			if(filter_var($_POST['ancienEmail'], FILTER_VALIDATE_EMAIL)){
+                $AncienEmail = $_POST['ancienEmail'];
+            }else{
+                $erreur = true;
+                echo 'Ancien mail invalide';
+            }
+
+
+            if(filter_var($_POST['nouvelEmail'], FILTER_VALIDATE_EMAIL)){
+                $NewEmail = $_POST['nouvelEmail'];
+            }else{
+                $erreur = true;
+                echo 'nouvel Email invalide';
+            }
+
+            $mdplength = strlen($_POST['password']);
+            if($mdplength >= 8){
+                $password = $_POST['password'];
+            }else{
+                $erreur = true;
                 $erreur = "Votre mot de passe doit contenir 8 caractères minimum.";
             }
+
+
+            $Cmdplength = strlen($_POST['Cpassword']);
+            if($Cmdplength >= 8){
+                $confirmPassword = $_POST['Cpassword'];
+            }else{
+                $erreur = true;
+                $erreur = "Votre mot de passe doit contenir 8 caractères minimum.";
+            }
+
+            if(!$erreur){
+                $requeteChangeMail = 'UPDATE Utilisateur SET Email = ? WHERE Identifiant = ? AND  Mot_de_passe = ?';
+                $requeteUpdateMail = $conn -> prepare($requeteChangeMail);
+                $requeteUpdateMail -> execute([$NewEmail, $idUserMyAccount, $password]);
+            }
+        }
+
         ?>
-
-        <?php
-        $Cmdplength = strlen($_POST['Cpassword']);
-            if($Cmdplength >= 8)
-            {
-                 }
-            else
-            {
-                $erreur = "Votre mot de passe doit contenir 8 caractères minimum.";
-            }
-            ?>
 
         
         <!--Formulaire de changement de mot de passe-->
         <form action="" method="post" align="center">
             <label for="ancienPassword"></label><br>
-            <input type="ancienPassword" id="ancienPassword" name="ancienPassword" placeholder ="Ancien mot de passe" required="ancienPassword" class="formulaire-myaccount">
+            <input type="password" id="ancienPassword" name="ancienPassword" placeholder ="Ancien mot de passe" required="ancienPassword" class="formulaire-myaccount">
             <label for="nouveauPassword" class="formulaire"></label><br>
-            <input type="nouveauPassword" id="nouveauPassword" name="nouveauPassword" placeholder ="Nouveau mot de passe" required="nouveauPassword" class="formulaire-myaccount">
+            <input type="password" id="nouveauPassword" name="nouveauPassword" placeholder ="Nouveau mot de passe" required="nouveauPassword" class="formulaire-myaccount">
             <label for="Cpassword" class="formulaire"></label><br>
-            <input type="password" id="Cpassword" name="Cpassword" placeholder ="Confirmez le nouveau mot de passe" required="Cpassword" class="formulaire-myaccount"><br>
+            <input type="password" id="Cpassword" name="ConfirmationPassword" placeholder ="Confirmez le nouveau mot de passe" required="Cpassword" class="formulaire-myaccount"><br>
             <button name="MyAccount" type="submit" value="HTML" class="MyAccountSpaceButton-myaccount">Création de la page mon espace</button>
         </form>
+        <?php
+        $erreur = false;
+
+
+		if (isset($_POST['MyAccount'])) {
+            $Amdplength = strlen($_POST['ancienPassword']);
+			if($Amdplength >= 8){
+                $AncienPass = $_POST['ancienPassword'];
+            }else{
+                $erreur = true;
+                $erreur='Mot de passe incorrect';
+            }
+
+            $Nmdplength = strlen($_POST['nouveauPassword']);
+            if($Nmdplength >= 8){
+                $NewPass = $_POST['nouveauPassword'];
+            }else{
+                $erreur = true;
+                $erreur = "Votre mot de passe doit contenir 8 caractères minimum.";
+            }
+
+            $Cmdplength = strlen($_POST['ConfirmationPassword']);
+            if($Cmdplength >= 8 AND $_POST['ConfirmationPassword'] == $_POST['nouveauPassword']){
+                $confirmPassword = $_POST['ConfirmationPassword'];
+            }else{
+                $erreur = true;
+                $erreur = "Votre mot de passe doit contenir 8 caractères minimum.";
+            }
+
+            if(!$erreur){
+                $requeteChangePassword = 'UPDATE Utilisateur SET Mot_de_passe = ? WHERE Identifiant =?';
+                $requeteUpdatePassword = $conn -> prepare($requeteChangePassword);
+                $requeteUpdatePassword -> execute([$NewPass, $idUserMyAccount,]);
+            }
+        }
+
+        ?>
     </main>
 
     <footer> 
